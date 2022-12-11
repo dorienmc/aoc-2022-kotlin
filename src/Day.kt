2771b09bd9988e2c81cb7db2db7a27f5fcaa9e3a
Dorien Lorijn - Meijer Cluwen <dorien.meijercluwen@nedap.com>
2022-12-11 20:13:39 +0100

    Empty day

---
+++ src/Day.kt
@@ -0,0 +1,19 @@
+// --- Day X: ??? ---
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0;
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0;
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day01_test")
+    check(part1(testInput) == 1)
+
+    val input = readInput("Day01")
+    part1(input).println()
+    part2(input).println()
+}
