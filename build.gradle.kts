7ef60eb2a8777f891b7b23fba513c0773073ca3a
Dorien Lorijn - Meijer Cluwen <dorien.meijercluwen@nedap.com>
2022-12-22 16:07:29 +0100

    Add day generation task
    Based on https://github.com/kotlin-hands-on/advent-of-code-kotlin-template/pull/17/files

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,4 +23,49 @@ tasks {
     wrapper {
         gradleVersion = "7.6"
     }
+
+    task("generateNextDay") {
+        doLast {
+            println("test")
+            val prevDayNum = fileTree("$projectDir/src").matching {
+                include("Day*.kt").exclude("Day.kt")
+            }.maxOf {
+                val (prevDayNum) = Regex("Day(\\d\\d)").find(it.name)!!.destructured
+                prevDayNum.toInt()
+            }
+            val newDayNum = String.format("%02d", prevDayNum + 1)
+            println("Generating files for day $newDayNum")
+            File("$projectDir/src/resources","Day${newDayNum}_test.txt").writeText("")
+            File("$projectDir/src/resources","Day${newDayNum}.txt").writeText("")
+            File("$projectDir/src", "Day$newDayNum.kt").writeText(
+                """
+//--- Day $newDayNum: ??? ---
+class Day$newDayNum : Day<Int> {
+    override val index = $newDayNum
+    
+    override fun part1(input: List<String>): Int {
+        return 0
+    }
+    override fun part2(input: List<String>): Int {
+        return 0
+    }
+}
+
+fun main() {
+    val day = Day$newDayNum()
+    
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput(day.index, true)
+    check(day.part1(testInput) == 0)
+    
+    val input = readInput(day.index)
+    day.part1(input).println()
+    
+    day.part2(testInput)
+    day.part2(input).println()
+}
+"""
+            )
+        }
+    }
 }
